f616c688b4307d818f673b1b581cffe0bfa3f740
alextomskras <dreamer.alex.toms@gmail.com>
2022-12-15 19:30:16 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testnavfragments/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.testnavfragments;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/testnavfragments/StartViewModel.java
@@ -0,0 +1,7 @@
+package com.example.testnavfragments;
+
+import androidx.lifecycle.ViewModel;
+
+public class StartViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
